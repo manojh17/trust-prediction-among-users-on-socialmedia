8875c54b70a5233d7aa6883bd1cc69553f2b1268
ManojhR <145860619+manojh17@users.noreply.github.com>
2025-08-03 22:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host:0.0.0.0,port:3001)
 
 
 
@@ -244,4 +244,4 @@ if __name__ == '__main__':
 #     return render_template("index.html", result=None)
 
 # if __name__ == "__main__":
-#     app.run(debug=True)
+#     app.run(debug=False,host:0.0.0.0,port:3001)
